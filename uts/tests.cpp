7f646516ccb98f37b749dda8c5defa2123bab89c
Mateusz Krajnik <mateusz.krajnik@protonmail.com>
2020-10-13 13:52:19 +0200

    Add source file main and tests main file

---
+++ uts/tests.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+#include <SFML/Graphics.hpp>
+
+int main(int argc, char *argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+    std::cout<<"TEST MAIN FILE"<<std::endl;
+    
+    return RUN_ALL_TESTS();
+}
