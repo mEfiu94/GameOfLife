4cfd460efa232d551d4e6f2c1cce0d1e00101f3a
Mateusz Krajnik <mateusz.krajnik@protonmail.com>
2021-02-03 20:48:25 +0100

    Add setting the state of Entity

--- inc/Entity.hpp
+++ inc/Entity.hpp
@@ -7,6 +7,7 @@ class Entity{
 public:
     Entity(StateOfEntity birthState):state_{birthState}{};
     StateOfEntity getState() const{return state_;};
+    void setState(StateOfEntity newState){state_=newState;};
     bool operator==(const Entity b)const{
         return getState()==b.getState();
     }
