5e5427c2952b1122227bb41e2cbd38eef2d26b7a
Mateusz Krajnik <mateusz.krajnik@protonmail.com>
2021-02-03 20:49:30 +0100

    Add tests reffering to setting and getting entity state

--- uts/BoardTest.hpp
+++ uts/BoardTest.hpp
@@ -22,6 +22,28 @@ TEST_F(BoardTest, ShouldReturnBoardSizeWhereXIsWidthAndYIsHeight)
 TEST_F(BoardTest, ShouldReturnVectorOfEntities)
 {
     Board TestBoard{10u,10u};
-    const std::vector<Entity> entities(100,StateOfEntity::Dead);
+    const boardMatrix entities(10,std::vector<Entity>(10,StateOfEntity::Dead));
     EXPECT_EQ(TestBoard.GetElements(),entities);
+}
+TEST_F(BoardTest, ShouldReturnDeadStateOfEntityAtSpecifiedCoordinates){
+    Board TestBoard{10u,10u};
+    auto actualState=TestBoard.GetStateAt(1u,1u);
+    EXPECT_EQ(StateOfEntity::Dead,actualState);
+    EXPECT_NE(StateOfEntity::Alive,actualState);
+}
+TEST_F(BoardTest, ShouldSetStateOfEntityAsAlive){
+    Board TestBoard{10u,10u};
+    TestBoard.SetStateAt(1u,1u,StateOfEntity::Alive);
+    EXPECT_EQ(StateOfEntity::Alive,TestBoard.GetStateAt(1u,1u));
+    EXPECT_EQ(StateOfEntity::Dead,TestBoard.GetStateAt(0u,0u));
+}
+TEST_F(BoardTest, ShouldThrowAnExceptionWhenGettingOutOfRange){
+    Board TestBoard{10u,10u};    
+    EXPECT_THROW(TestBoard.GetStateAt(10,9),std::out_of_range);
+    EXPECT_NO_THROW(TestBoard.GetStateAt(9,9));
+}
+TEST_F(BoardTest,ShouldThrowAnExceptionWhenSettingOutOfRange){
+    Board TestBoard{10u,10u};    
+    EXPECT_THROW(TestBoard.SetStateAt(10,9,StateOfEntity::Alive),std::out_of_range);
+    EXPECT_NO_THROW(TestBoard.SetStateAt(9,9,StateOfEntity::Alive));
 }
