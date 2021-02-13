a81cb5b5659fd5a85c4ca98582cb2d9094b735e8
mEfiu94 <mateusz.krajnik@protonmail.com>
2021-02-13 22:22:00 +0100

    Add functionality od displaying window with rectangles as entities and updating it

--- inc/Renderer.hpp
+++ inc/Renderer.hpp
@@ -5,10 +5,36 @@ class Renderer {
  public:
   Renderer() = delete;
   explicit Renderer(Board& board) {
-    mainWindow_.create(sf::VideoMode(board.getSize().x, board.getSize().y),
-                       "GameOfLife", sf::Style::Close);
-    boardPtr_ = std::make_shared<Board>(board);
-    mainWindow_.setFramerateLimit(144);
+    mainWindow_.create(
+        sf::VideoMode(board.getSize().x * 10, board.getSize().y * 10),
+        "GameOfLife", sf::Style::Close);
+    boardPtr_ = std::make_unique<Board>(board);
+    mainWindow_.setFramerateLimit(60);
+    for (size_t i = 0; i < (board.getSize().x); i++)
+      for (size_t j = 0; j < (board.getSize().y); j++) {
+        auto shape = sf::RectangleShape(sf::Vector2f(10.0f, 10.0f));
+        shape.setFillColor(sf::Color::White);
+        shape.setOutlineColor(sf::Color::Black);
+        shape.setOutlineThickness(-0.5f);
+        shape.setPosition(10.f * i, 10.f * j);
+        entitiesShapeVector_.push_back(shape);
+      }
+  }
+  void updateRectangles() {
+    auto iter = std::begin(entitiesShapeVector_);
+    for (auto elem : boardPtr_->GetElements())
+      for (auto elem2 : elem) {
+        if (elem2.getState() == StateOfEntity::Dead) {
+          iter->setFillColor(sf::Color::Black);
+        } else {
+          iter->setFillColor(sf::Color::White);
+        }
+        iter->setOutlineColor(sf::Color::White);
+        if (iter <= std::end(entitiesShapeVector_))
+          ++iter;
+        else
+          return;
+      }
   }
   void showWindow() {
     while (mainWindow_.isOpen()) {
@@ -16,12 +42,15 @@ class Renderer {
       while (mainWindow_.pollEvent(event)) {
         if (event.type == sf::Event::Closed) mainWindow_.close();
       }
-      mainWindow_.clear(sf::Color::Black);            
+      mainWindow_.clear(sf::Color::Black);
+      updateRectangles();
+      for (auto elem : entitiesShapeVector_) mainWindow_.draw(elem);
       mainWindow_.display();
     }
   }
 
  private:
-  std::shared_ptr<Board> boardPtr_;
-  sf::RenderWindow mainWindow_; 
+  std::unique_ptr<Board> boardPtr_;
+  sf::RenderWindow mainWindow_;
+  std::vector<sf::RectangleShape> entitiesShapeVector_;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <memory>
+#include <random>
 
 #include "../inc/Renderer.hpp"
 int main() {
-  Board GameBoard(800, 600);
+  Board GameBoard(192, 108);
+  GameBoard.InvertStateAt(9, 0);
   Renderer Render(GameBoard);
   Render.showWindow();
   return 0;
