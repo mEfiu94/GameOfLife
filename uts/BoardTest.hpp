a58bc6b210dc85ec0c0faa5cd10bb09fc68765c9
mEfiu94 <mateusz.krajnik@protonmail.com>
2021-02-15 14:49:00 +0100

    Move TestBoard to BoardTest class so that UT don need to make new object

--- uts/BoardTest.hpp
+++ uts/BoardTest.hpp
@@ -3,6 +3,7 @@
 
 class BoardTest : public ::testing::Test {
  protected:
+  Board TestBoard = Board{10u, 20u};
   void SetUp() override{};
   void TearDown() override{};
 
@@ -19,29 +20,25 @@ TEST_F(BoardTest, ShouldReturnBoardSizeWhereXIsWidthAndYIsHeight) {
   ASSERT_NEAR(plansza.getSize().y, y, 0.00001);
 }
 TEST_F(BoardTest, ShouldReturnVectorOfEntities) {
-  Board TestBoard{10u, 10u};
-  const boardMatrix entities(10, std::vector<Entity>(10, StateOfEntity::Dead));
+  const boardMatrix entities(
+      10, std::vector<Entity>(20, Entity(StateOfEntity::Dead)));
   EXPECT_EQ(TestBoard.GetElements(), entities);
 }
 TEST_F(BoardTest, ShouldReturnDeadStateOfEntityAtSpecifiedCoordinates) {
-  Board TestBoard{10u, 10u};
   auto actualState = TestBoard.GetStateAt(1u, 1u);
   EXPECT_EQ(StateOfEntity::Dead, actualState);
   EXPECT_NE(StateOfEntity::Alive, actualState);
 }
 TEST_F(BoardTest, ShouldSetStateOfEntityAsAlive) {
-  Board TestBoard{10u, 10u};
   TestBoard.SetStateAt(1u, 1u, StateOfEntity::Alive);
   EXPECT_EQ(StateOfEntity::Alive, TestBoard.GetStateAt(1u, 1u));
   EXPECT_EQ(StateOfEntity::Dead, TestBoard.GetStateAt(0u, 0u));
 }
 TEST_F(BoardTest, ShouldThrowAnExceptionWhenGettingOutOfRange) {
-  Board TestBoard{10u, 10u};
   EXPECT_THROW(TestBoard.GetStateAt(10, 9), std::out_of_range);
   EXPECT_NO_THROW(TestBoard.GetStateAt(9, 9));
 }
 TEST_F(BoardTest, ShouldThrowAnExceptionWhenSettingOutOfRange) {
-  Board TestBoard{10u, 10u};
   EXPECT_THROW(TestBoard.SetStateAt(10, 9, StateOfEntity::Alive),
                std::out_of_range);
   EXPECT_NO_THROW(TestBoard.SetStateAt(9, 9, StateOfEntity::Alive));
