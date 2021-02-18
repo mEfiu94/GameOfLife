e5088a1651110aae608f6c62f5ecc4075ac77a69
mEfiu94 <mateusz.krajnik@protonmail.com>
2021-02-18 13:46:41 +0100

    Add uniform distribution for the first population

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,21 @@
 #include <random>
 
 #include "../inc/Renderer.hpp"
+#include "../inc/World.hpp"
 int main() {
-  Board GameBoard(192, 108);
-  GameBoard.InvertStateAt(9, 0);
-  Renderer Render(GameBoard);
+  auto BoardPtr = std::make_shared<Board>(75, 75);
+  World TestWorld(BoardPtr);
+  Renderer Render(BoardPtr);
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> distrib(0, 74);
+  std::uniform_int_distribution<> distrib2(0, 1);
+  for (int i = 0; i <= 74*74; i++) {
+    if (distrib2(gen))
+      BoardPtr->SetStateAt(distrib(gen), distrib(gen), StateOfEntity::Alive);
+    else
+      BoardPtr->SetStateAt(distrib(gen), distrib(gen), StateOfEntity::Dead);
+  }
   Render.showWindow();
   return 0;
 }
