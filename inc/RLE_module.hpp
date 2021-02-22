12e8e410f8e319c63a758727d55c4cc85863eaf0
mEfiu94 <mateusz.krajnik@protonmail.com>
2021-02-22 16:20:14 +0100

    Begin work on the parse function in RLE Module

--- inc/RLE_module.hpp
+++ inc/RLE_module.hpp
@@ -1,10 +1,17 @@
 #pragma once
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <vector>
 
-class RLE_module{
-  public:
-    bool open(const char* filename);
-  private:
-    std::ifstream FileToRead_;
+#include "../inc/Entity.hpp"
+class RLE_module {
+ public:
+  bool open(const char* filename);
+  std::vector<Entity> parse();
+  unsigned int GetWidth() const { return width; };
+  unsigned int GetHeight() const { return height; };
+
+ private:
+  unsigned int width = 0, height = 0;
+  std::ifstream FileToRead_;
 };
--- src/RLE_module.cpp
+++ src/RLE_module.cpp
@@ -1,6 +1,34 @@
 #include "../inc/RLE_module.hpp"
 
-bool RLE_module::open(const char* filename){
-  FileToRead_.open(filename,std::ios::in);
+#include <cstdlib>
+#include <regex>
+bool RLE_module::open(const char* filename) {
+  FileToRead_.open(filename, std::ios::in);
   return FileToRead_.is_open();
+}
+std::vector<Entity> RLE_module::parse() {
+  std::string line;
+  auto numbers_regex =
+      std::regex("\\b([1-9]|[1-9][0-9]|[1-9][0-9][0-9]|1000)\\b");
+  while (!FileToRead_.eof()) {
+    std::getline(FileToRead_, line);
+    if (line.starts_with('x')) {
+      auto reg_begin =
+          std::sregex_iterator(line.begin(), line.end(), numbers_regex);
+      auto reg_end = std::sregex_iterator();
+      auto i = reg_begin;
+      for (auto k = 0; k < 2; k++) {
+        std::smatch match = *i;
+        const std::string number = match.str();
+        switch (k) {
+          case 0:
+            width = std::stoul(number);
+          case 1:
+            height = std::stoul(number);
+        }
+        if (i != reg_end) i++;
+      }
+    }
+  }
+  return std::vector<Entity>();
 }
